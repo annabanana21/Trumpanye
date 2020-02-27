a889eda59f3c711a63e9dd474de8d282fc7fed68
Anna <av.mastoris@gmail.com>
2020-02-27 16:54:57 -0500

    Created hero section -mainpage

---
+++ client/src/pages/main.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import React from 'react';
+import Hero from '../components/Hero/hero'
+
+function MainPage() {
+    return (
+        <Hero />
+    )
+}
+
+
+export default MainPage;
