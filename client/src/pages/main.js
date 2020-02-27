1264b296078e83b53cc25b106d97fcc3a0dc1815
Anna <av.mastoris@gmail.com>
2020-02-27 17:34:36 -0500

    Structured main page

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import React from 'react';
 import Hero from '../components/Hero/hero'
+import UserForm from '../components/UserForm/userform';
 
 function MainPage() {
     return (
-        <Hero />
+        <main>
+            <Hero />
+            <UserForm />
+        </main>
     )
 }
 
