f84def37743ddfb787108850100780054b6b1d7c
rachidtoufique <58738363+rachidtoufique@users.noreply.github.com>
2020-02-28 12:05:59 -0500

    updated gif.js

--- server/gifs.js
+++ server/gifs.js
@@ -1,17 +1,25 @@
 module.exports = [
-    "https://i.imgur.com/BAoQQGi.gif",
-    "https://i.imgur.com/yyQ7ynv.gif",
+    "https://i.imgur.com/6ezlyWB.gif",
     "https://i.imgur.com/cjSzWXp.gif",
-    "https://i.imgur.com/zlhavwn.gif",
-    "https://i.imgur.com/bc6XQwN.gif",
-    "https://i.imgur.com/W6uFVUm.gif",
-    "https://i.imgur.com/uenJp7g.gif",
-    "https://i.imgur.com/yAHTrdu.gif",
+    "https://i.imgur.com/YakNODy.gif",
+    "https://i.imgur.com/UKGuYfJ.gif",
     "https://i.imgur.com/yuDwP58.gif",
-    "https://i.imgur.com/jGiiyTh.gif",
-    "https://i.imgur.com/5eVLKvo.gif",
-    "https://i.imgur.com/KraPDff.gif",
-    "https://i.imgur.com/6ezlyWB.gif"
+    "https://i.imgur.com/e1Wbaww.gif",
+    "https://i.imgur.com/QruXc5D.gif",
+    "https://i.imgur.com/brFrUZU.gif",
+    "https://i.imgur.com/fja9Mdq.gif",
+    "https://i.imgur.com/1EIEUQE.gif",
+    "https://i.imgur.com/GFeIdqW.gif",
+    "https://i.imgur.com/h3k2hS8.gif",
+    "https://i.imgur.com/fwoWUat.gif",
+    "https://i.imgur.com/6nsTE5I.gif",
+    "https://i.imgur.com/vGj0SEI.gif",
+    "https://i.imgur.com/nfuYikw.gif",
+    "https://i.imgur.com/8m9UZx2.gif",
+    "https://i.imgur.com/kz9IX6u.gif",
+    "https://i.imgur.com/q4Ll5I6.gif"
+
+
 
 
 ]
