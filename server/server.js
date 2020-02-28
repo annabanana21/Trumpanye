70e472c8f35cd1bb329c180c72c3e1af7cd661ca
rachidtoufique <58738363+rachidtoufique@users.noreply.github.com>
2020-02-28 11:20:01 -0500

    added cors and updated gif.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const gifs = require('./gifs');
+const cors = require('cors');
 
+app.use(cors());
 app.listen(5000, function () {
-    console.log('the TRUMP is running');
+    console.log('the TRUMP is running on 5000');
 });
 
 app.get('/', function (req, res) {
